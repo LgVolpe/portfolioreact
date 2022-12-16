154a6b9d3b4a1fe47f9497c8c40c3901a2620bef
Luis Guilherme Volpe <104277176+LgVolpe@users.noreply.github.com>
2022-12-15 21:16:35 -0300

    Home Component 15/12

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -47,7 +47,7 @@ export const Header = () => {
 
           <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
         </div>
-        
+
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i class="uil uil-apps"></i>
         </div>
