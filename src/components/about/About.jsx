3b963f0ab4e5eca6a91d873278f57487da5eb3f9
Luis Guilherme Volpe <104277176+LgVolpe@users.noreply.github.com>
2022-12-19 21:47:41 -0300

    About me

---
+++ src/components/about/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./about.css";
+// eslint-disable-next-line
+import AboutImg from "../../assets/profile.jpg.png";
+
+
+
+const About = () => {
+    return (
+        <section className="about section" id="about">
+            <h2 className="section__title">About Me</h2>
+            <span className="section__subtitle">My Introduction</span>
+
+            <div className="about__container container grid">
+                <img src="{AboutImg}" alt="" className="about__img" />
+
+                <div className="about__data"></div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
